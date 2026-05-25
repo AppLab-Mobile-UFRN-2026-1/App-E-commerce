8296d89bfda9e6d515748c54aebaf1d76ec56ed1
Leonardo Nadson Oliveira de Medeiros <leonardo.medeiros@agiltec.com>
2026-05-24 23:40:09 -0300

    Criação de um Tema para o App

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,24 +1,14 @@
 import 'package:applab_ecommerce/src/screens/login_screen.dart';
+import 'package:applab_ecommerce/src/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class AppLabEcommerceApp extends StatelessWidget {
   const AppLabEcommerceApp({super.key});
 
- @override
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        colorScheme: const ColorScheme.dark(
-          primary: Color(0xFF290349),
-          onPrimary: Colors.white,
-          secondary: Color(0xFF4A032F),
-          onSecondary: Colors.white,
-          surface: Color(0xFFF8F1FF),
-          onSurface: Color(0xFF201429),
-          error: Color(0xFFFF5A7A),
-          onError: Colors.white,
-        ),
-      ),
+      theme: AppTheme.light,
       debugShowCheckedModeBanner: false,
       home: const LoginScreen(),
     );
